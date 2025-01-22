d9838e32a7b5b633c3d3990b39e1247b53e7e965
Søren Starck <soren.starck@gmail.com>
2025-01-22 12:47:35 +0100

    feat : navbar working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
+"use client";
+
+import { useState } from 'react';
 import Navbar from '../components/Navbar';
 import Home from '../components/home';
+import About from '../components/About';
+import Contact from '../components/Contact';
 
 export default function Page() {
+    const [currentPage, setCurrentPage] = useState('home');
+
+    const renderPage = () => {
+        switch (currentPage) {
+            case 'home':
+                return <Home />;
+            case 'about':
+                return <About />;
+            case 'contact':
+                return <Contact />;
+            default:
+                return <Home />;
+        }
+    };
+
     return (
         <div className='w-full h-full'>
-            <Navbar />
-            <Home />
+            <Navbar setCurrentPage={setCurrentPage} />
+            {renderPage()}
         </div>
     );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,16 @@
-import Link from 'next/link';
 import { Button } from './ui/button';
 
-export default function Navbar() {
+export default function Navbar({ setCurrentPage }) {
     return (
         <div className='flex justify-center w-fit backdrop-blur-lg mx-auto mt-4 px-2 py-1 rounded-xl shadow-lg gap-4'>
-            <Button variant='link' className='font-bold'>
-                <Link href="/">Home</Link>
+            <Button variant='link' className='font-bold' onClick={() => setCurrentPage('home')}>
+                Home
             </Button>
-            <Button variant='link' className='font-bold'>
-                <Link href="/about">About</Link>
+            <Button variant='link' className='font-bold' onClick={() => setCurrentPage('about')}>
+                About
             </Button>
-            <Button variant='link' className='font-bold'>
-                <Link href="/contact">Contact</Link>
+            <Button variant='link' className='font-bold' onClick={() => setCurrentPage('contact')}>
+                Contact
             </Button>
         </div>
     );
