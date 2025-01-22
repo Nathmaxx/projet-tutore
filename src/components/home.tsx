7ff1fc9ead6bb9e57aa986be6e3d4ed86eb655d3
juliett52 <juliettrojas01@gmail.com>
2025-01-22 12:47:24 +0100

    Added Accordion for website use info as well as welcome text

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,9 +1,30 @@
-
+import {Accordion} from "@/components/ui/accordion";
+import {AccordionItem} from "@/components/ui/accordion";
+import {AccordionTrigger} from "@/components/ui/accordion";
+import {AccordionContent} from "@/components/ui/accordion";
 
 export default function Home() {
 	return (
 		<div>
-			Hello World
+			<p>
+				<p class={"text-2xl"}>Bienvenue, sur " "</p>
+				<Accordion type="single" collapsible>
+					<AccordionItem value="item-1">
+						<AccordionTrigger>Quelle est l'objectif?</AccordionTrigger>
+						<AccordionContent>
+							Permettre l'analyse facile de données en rapport avec
+							la consommation d'électricité dans la métropole de Grand Lyon.
+						</AccordionContent>
+					</AccordionItem>
+					<AccordionItem value={"item-2"}>
+						<AccordionTrigger>Quelles analyses sont à disposition?</AccordionTrigger>
+						<AccordionContent>
+							On propose une analyse sous forme d'une carte interactive puis
+							une analyse sous forme de graphique.
+						</AccordionContent>
+					</AccordionItem>
+				</Accordion>
+			</p>
 		</div>
 	);
 }
