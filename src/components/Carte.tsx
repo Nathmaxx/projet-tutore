4e081a2a2a7f9bf20f66ef7927f12fdd2391b9b7
Søren Starck <soren.starck@gmail.com>
2025-01-23 11:12:13 +0100

    feat : better looking map

--- src/components/Carte.tsx
+++ src/components/Carte.tsx
@@ -1,40 +1,53 @@
-import {Combobox} from "@/components/ComboBox";
-import {Card, CardHeader} from "@/components/ui/card";
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import { Card, CardContent, CardHeader } from './ui/card';
+import { Combobox } from './ComboBox';
 
 export default function Carte() {
     const mapContainer = useRef(null);
+    const [API_KEY] = useState('INdXbZgYY8GqK1IGryTJ');
+    const [viewState] = useState({
+        center: [2.213749, 46.227638],
+        zoom: 5,
+        pitch: 40
+    });
 
     useEffect(() => {
         if (mapContainer.current) {
             const map = new maplibregl.Map({
                 container: mapContainer.current,
-                style: 'https://demotiles.maplibre.org/style.json',
-                center: [0, 0],
-                zoom: 2,
+                style: `https://api.maptiler.com/maps/toner-v2/style.json?key=${API_KEY}`,
+                center: viewState.center,
+                zoom: viewState.zoom,
+                pitch: viewState.pitch
             });
+
+            const marker = new maplibregl.Marker()
+                .setLngLat([5.0000, 45.7667])
+                .addTo(map);
 
             return () => map.remove();
         }
-    }, []);
+    }, [API_KEY, viewState]);
 
     return (
         <Card className="w-4/5 h-fit mx-auto">
             <CardHeader>
                 <h1 className="text-2xl font-bold">Map page</h1>
             </CardHeader>
-            <CardHeader>
+            <CardContent>
                 <div className='font-bold'>
                     Page de la carte
                 </div>
-                <div ref={mapContainer} className="w-full h-96" />
                 <div className="flex items-center space-x-4 text-sm m-10">
                     <div>Début: <Combobox /></div>
                     <div>Fin: <Combobox /></div>
                 </div>
-            </CardHeader>
+                <div className='rounded-lg'>
+                    <div ref={mapContainer} className="w-full h-96" />
+                </div>
+            </CardContent>
         </Card>
     );
 }
