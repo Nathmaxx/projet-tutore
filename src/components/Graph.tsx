5eb1ea7d64a276cd8f91ae0bca1c3a4613d521ec
juliett52 <juliettrojas01@gmail.com>
2025-01-27 16:19:38 +0100

    adding of variables RBG to modify graphs.

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,18 +1,25 @@
 import { useState, useEffect } from 'react';
 import { Card, CardContent, CardHeader } from "./ui/card";
 import { ComboBoxYear } from "@/components/ComboBoxYear";
-import { ComboBoxGraphType } from "@/components/ComboBoxGraphType";
 import { BarCharter } from "@/components/BarCharter";
 import { RadarCharter } from './RadarCharter';
 import { DoughnutCharter } from './DoughnutCharter';
-import { LineChart } from 'lucide-react';
-import { LineCharter } from './LineCharter';
+import {LineCharter} from "@/components/LineCharter";
+
+const rElec = 115;
+const gElec = 181;
+const bElec = 255;
+const transparencyElec = 0.5;
+
+const rGaz = 255;
+const gGaz = 183;
+const bGaz = 52;
+const transparencyGaz = 0.5;
 import { Label } from './ui/label';
 
 export default function Graph() {
     const [startYear, setStartYear] = useState("2018");
     const [endYear, setEndYear] = useState("2019");
-    const [graphType, setGraphType] = useState(null);
     const [error, setError] = useState("");
 
     const handleStartYearChange = (year: string) => {
@@ -33,21 +40,16 @@ export default function Graph() {
         }
     };
 
-    const handleGraphTypeChange = (type: string) => {
-        setGraphType(type);
-    };
-
-    useEffect(() => {
-        console.log(graphType)
-    }, [graphType])
-
     return (
         <div className="w-full h-full px-4 flex flex-col gap-4">
 
             <div className='flex flex-row gap-4 h-1/6'>
                 <Card className="w-1/3">
                     <CardContent>
-                        <BarCharter startYear={startYear} endYear={endYear} />
+                        <BarCharter startYear={startYear} endYear={endYear}
+                                    rElec={rElec} gElec={gElec} bElec={bElec} transparencyElec={transparencyElec}
+                                    rGaz={rGaz} gGaz={gGaz} bGaz={bGaz} transparencyGaz={transparencyGaz}
+                        />
                     </CardContent>
                 </Card>
 
@@ -85,13 +87,19 @@ export default function Graph() {
                 <div className='flex flex-col gap-4 w-2/3'>
                     <Card className="w-full" style={{ height: '-webkit-fill-available' }}>
                         <CardContent className='mt-4'>
-                            <LineCharter startYear={startYear} endYear={endYear} />
+                            <LineCharter startYear={startYear} endYear={endYear}
+                                         rElec={rElec} gElec={gElec} bElec={bElec} transparencyElec={transparencyElec}
+                                         rGaz={rGaz} gGaz={gGaz} bGaz={bGaz} transparencyGaz={transparencyGaz}
+                            />
                         </CardContent>
                     </Card>
                     <div className="flex flex-row gap-4">
                         <Card className="w-full" style={{ height: '-webkit-fill-available' }}>
                             <CardContent>
-                                <RadarCharter startYear={startYear} endYear={endYear} />
+                                <RadarCharter startYear={startYear} endYear={endYear}
+                                              rElec={rElec} gElec={gElec} bElec={bElec} transparencyElec={transparencyElec}
+                                              rGaz={rGaz} gGaz={gGaz} bGaz={bGaz} transparencyGaz={transparencyGaz}
+                                />
                             </CardContent>
                         </Card>
                         <Card className="w-full" style={{ height: '-webkit-fill-available' }}>
