e3eda10744f310702531b41e1961375cb42f4092
Søren Starck <soren.starck@gmail.com>
2025-01-28 14:56:12 +0100

    feat : added router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Home from '../components/home';
@@ -8,36 +9,50 @@ import Carte from '../components/Carte';
 import Graph from '../components/Graph';
 import Contact from '../components/Contact';
 
-import {AnimatedNoise} from "../components/ui/AnimatedNoise";
+import { AnimatedNoise } from "../components/ui/AnimatedNoise";
 
 export default function Page() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
     const [currentPage, setCurrentPage] = useState('home');
+
+    useEffect(() => {
+        const page = searchParams.get('page');
+        if (page) {
+            setCurrentPage(page);
+        }
+    }, [searchParams]);
+
+    const handlePageChange = (page: string) => {
+        setCurrentPage(page);
+        router.push(`/?page=${page}`);
+    };
 
     const renderPage = () => {
         switch (currentPage) {
             case 'home':
-                return <Home/>;
+                return <Home />;
             case 'carte':
-                return <Carte/>;
+                return <Carte />;
             case 'graph':
-                return <Graph/>;
+                return <Graph />;
             case 'contact':
-                return <Contact/>;
+                return <Contact />;
             default:
-                return <Home/>;
+                return <Home />;
         }
     };
 
     return (
         <div className='w-full h-screen flex flex-col justify-between overflow-hidden'>
             <div className="absolute h-screen w-screen -z-10 overflow-hidden">
-                <AnimatedNoise opacity={0.07}/>
+                <AnimatedNoise opacity={0.07} />
             </div>
-            <Navbar setCurrentPage={setCurrentPage}/>
+            <Navbar setCurrentPage={handlePageChange} />
             <div className='flex-grow h-full'>
                 {renderPage()}
             </div>
-            <Footer />
+            {currentPage !== 'graph' && <Footer />}
         </div>
     );
 }
