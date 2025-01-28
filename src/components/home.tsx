fa16faa471224760e9f82f690c321c06a1fb0845
juliett52 <juliettrojas01@gmail.com>
2025-01-28 15:46:19 +0100

    adding of image of map of lyon and also definition of website name

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,35 +1,39 @@
 import {
-	Accordion,
-	AccordionTrigger,
-	AccordionContent,
-	AccordionItem
+    Accordion,
+    AccordionTrigger,
+    AccordionContent,
+    AccordionItem
 } from "@/components/ui/accordion";
-import { Card, CardContent, CardHeader } from "./ui/card";
+import {Card, CardContent, CardHeader} from "./ui/card";
+import Image from "next/image";
+
+import MyImage from "../img/Carte_Métropole_du_Grand_Lyon.png";
 
 export default function Home() {
-	return (
-		<Card className="w-4/5 h-fit mx-auto">
-			<CardHeader>
-				<p className={"text-2xl"}>Bienvenue, sur " "</p>
-			</CardHeader>
-			<CardContent>
-				<Accordion type="single" collapsible>
-					<AccordionItem value="item-1">
-						<AccordionTrigger>Quelle est l'objectif?</AccordionTrigger>
-						<AccordionContent>
-							Permettre l'analyse facile de données en rapport avec
-							la consommation d'électricité dans la métropole de Grand Lyon.
-						</AccordionContent>
-					</AccordionItem>
-					<AccordionItem value={"item-2"}>
-						<AccordionTrigger>Quelles analyses sont à disposition?</AccordionTrigger>
-						<AccordionContent>
-							On propose une analyse sous forme d'une carte interactive puis
-							une analyse sous forme de graphique.
-						</AccordionContent>
-					</AccordionItem>
-				</Accordion>
-			</CardContent>
-		</Card>
-	);
+    return (
+        <Card className="w-4/5 h-fit mx-auto">
+            <CardHeader className="w-full flex justify-center items-center">
+                <div className="text-2xl pb-3" >Bienvenue, sur DataWatt Lyon</div>
+                <Image className="inline-block align-middle" src={MyImage} width={300} height={300} alt="My PNG Image"/>
+            </CardHeader>
+            <CardContent>
+                <Accordion type="single" collapsible>
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger>Quel est l&apos;objectif?</AccordionTrigger>
+                        <AccordionContent>
+                            Permettre l&apos;analyse facile de données en rapport avec
+                            la consommation d&apos;électricité dans la métropole de Grand Lyon.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value={"item-2"}>
+                        <AccordionTrigger>Quelles analyses sont à disposition?</AccordionTrigger>
+                        <AccordionContent>
+                            On propose une analyse sous forme d&apos;une carte interactive puis
+                            une analyse sous forme de graphique.
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+            </CardContent>
+        </Card>
+    );
 }
