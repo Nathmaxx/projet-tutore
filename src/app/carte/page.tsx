c01b0ae3e9406a3246f162e94464d0a8dd73efd1
Søren Starck <soren.starck@gmail.com>
2025-01-29 10:08:06 +0100

    feat : move map file

--- src/app/carte/page.tsx
+++ src/app/carte/page.tsx
@@ -1,11 +1,14 @@
-'use client'
-
+"use client";
 import { useEffect, useRef, useState } from 'react';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import { Card, CardContent, CardHeader } from '../../components/ui/card';
-import { ComboBoxYear } from '../../components/ComboBoxYear';
-import Navbar from '@/components/Navbar';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { ComboBoxYear } from '@/components/ComboBoxYear';
+import { Popover, PopoverTrigger, PopoverContent } from '@/components/ui/popover';
+import { Button } from '@/components/ui/button';
+import { Command, CommandInput, CommandList, CommandItem, CommandEmpty, CommandGroup } from '@/components/ui/command';
+import { Check, ChevronsUpDown } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
 const API_URL = process.env.API_URL;
 const API_KEY = process.env.NRG_LYON_API_KEY;
@@ -14,15 +17,16 @@ export default function Carte() {
     const [MAP_SKIN_API_KEY] = useState('INdXbZgYY8GqK1IGryTJ');
     const mapContainer = useRef(null);
     const [viewState] = useState<{ center: [number, number]; zoom: number; pitch: number }>({
-        center: [5.0000, 45.7667],
-        zoom: 10,
+        center: [4.83397, 45.76748],
+        zoom: 13,
         pitch: 40
     });
 
     const [communes, setCommunes] = useState<string[]>([]);
     const [selectedCommune, setSelectedCommune] = useState<string>('');
+    const [selectedYear, setSelectedYear] = useState<number | null>(null);
     const [map, setMap] = useState<maplibregl.Map | null>(null);
-    const [markers, setMarkers] = useState<maplibregl.Marker[]>([]);
+    const [open, setOpen] = useState(false);
 
     useEffect(() => {
         if (mapContainer.current) {
@@ -40,37 +44,7 @@ export default function Carte() {
     }, [MAP_SKIN_API_KEY, viewState]);
 
     useEffect(() => {
-        const fetchData = async () => {
-            const tokenOptions = {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${API_KEY}`,
-                },
-            };
-
-            try {
-                const response = await fetch(`${API_URL}parcelles`, tokenOptions);
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                const data = await response.json();
-                console.log(data);
-
-                const uniqueCommunes = Array.from(new Set(data.map((parcelle: any) => parcelle.commune)));
-                setCommunes(uniqueCommunes);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
-    }, [API_KEY]);
-
-    useEffect(() => {
-        if (map && selectedCommune) {
-            markers.forEach(marker => marker.remove());
-
+        if (selectedYear) {
             const fetchData = async () => {
                 const tokenOptions = {
                     method: 'GET',
@@ -88,28 +62,8 @@ export default function Carte() {
                     const data = await response.json();
                     console.log(data);
 
-                    let markerCount = 0;
-                    const newMarkers: maplibregl.Marker[] = [];
-                    data.forEach((parcelle: any) => {
-                        if (markerCount >= 500) return;
-
-                        if (parcelle.commune === selectedCommune) {
-                            const coordinates = JSON.parse(parcelle.coordinates);
-                            const lng = parseFloat(coordinates.lng);
-                            const lat = parseFloat(coordinates.lat);
-
-                            if (!isNaN(lng) && !isNaN(lat)) {
-                                const marker = new maplibregl.Marker()
-                                    .setLngLat([lng, lat])
-                                    .addTo(map);
-                                newMarkers.push(marker);
-                                markerCount++;
-                            } else {
-                                //console.error(`Invalid coordinates for parcelle ${parcelle.id_parcelle}:`, coordinates);
-                            }
-                        }
-                    });
-                    setMarkers(newMarkers);
+                    const uniqueCommunes = Array.from(new Set(data.map((parcelle: any) => parcelle.commune)));
+                    setCommunes(uniqueCommunes);
                 } catch (error) {
                     console.error('Error fetching data:', error);
                 }
@@ -117,30 +71,172 @@ export default function Carte() {
 
             fetchData();
         }
-    }, [selectedCommune, map]);
+    }, [API_KEY, selectedYear]);
+
+    useEffect(() => {
+        if (map && selectedCommune && selectedYear) {
+            if (map.getSource('parcelles')) {
+                map.removeLayer('parcelles-layer');
+                map.removeSource('parcelles');
+            }
+
+            const fetchData = async () => {
+                const tokenOptions = {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${API_KEY}`,
+                    },
+                };
+
+                try {
+                    const response = await fetch(`${API_URL}parcelles?annee=${selectedYear}`, tokenOptions);
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+                    const data = await response.json();
+                    console.log(data);
+
+                    const communeData = data.filter((parcelle: any) => parcelle.commune === selectedCommune);
+                    const avgConsoElec = communeData.reduce((sum: number, parcelle: any) => sum + (parcelle.conso_elec || 0), 0) / communeData.length;
+
+                    const features = communeData
+                        .slice(0, 9999)
+                        .map((parcelle: any) => {
+                            const coordinates = JSON.parse(parcelle.coordinates);
+                            return {
+                                type: 'Feature',
+                                geometry: {
+                                    type: 'Point',
+                                    coordinates: [parseFloat(coordinates.lng), parseFloat(coordinates.lat)]
+                                },
+                                properties: {
+                                    id: parcelle.id_parcelle,
+                                    surface: (parcelle.majic_surf_habitable_parcelle || 30) / 10,
+                                    conso_elec: parcelle.conso_elec || 0,
+                                    avgConsoElec: avgConsoElec
+                                }
+                            };
+                        });
+
+                    const geojson = {
+                        type: 'FeatureCollection',
+                        features: features
+                    };
+
+                    map.addSource('parcelles', {
+                        type: 'geojson',
+                        data: geojson
+                    });
+
+                    map.addLayer({
+                        id: 'parcelles-layer',
+                        type: 'circle',
+                        source: 'parcelles',
+                        paint: {
+                            'circle-radius': [
+                                'interpolate',
+                                ['linear'],
+                                ['get', 'surface'],
+                                0, 3,
+                                100, 10,
+                                1000, 20
+                            ],
+                            'circle-color': [
+                                'interpolate',
+                                ['linear'],
+                                ['/', ['get', 'conso_elec'], ['get', 'avgConsoElec']],
+                                0, '#22c55e',
+                                1, '#f59e0b',
+                                2, '#ef4444'
+                            ],
+                            'circle-stroke-width': 0.2,
+                            'circle-stroke-color': '#000000'
+                        }
+                    });
+
+                    const lngLat = features
+                        .map((feature: any) => feature.geometry.coordinates)
+                        .filter((coord: number[]) => coord && coord.length === 2 && coord[0] !== null && coord[1] !== null);
+
+                    if (lngLat.length > 0) {
+                        const randomIndex = Math.floor(Math.random() * lngLat.length);
+                        const randomCoord = lngLat[randomIndex];
+
+                        if (randomCoord && randomCoord.length === 2 && !isNaN(randomCoord[0]) && !isNaN(randomCoord[1])) {
+                            map.flyTo({
+                                center: [randomCoord[0], randomCoord[1]],
+                                essential: true
+                            });
+                            console.log('Center:', randomCoord[0], randomCoord[1]);
+                        } else {
+                            console.error('Invalid random coordinate:', randomCoord);
+                        }
+                    }
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                }
+            };
+
+            fetchData();
+        }
+    }, [selectedCommune, selectedYear, map]);
 
     return (
         <div className="w-full h-full px-4">
-            <Navbar />
             <Card className="w-full h-full">
                 <CardHeader>
                     <h1 className="text-2xl font-bold">Carte interactive de la Métropole de Lyon</h1>
                 </CardHeader>
                 <CardContent>
                     <div className="flex items-center space-x-4 text-sm m-4">
-                        <div>Début: <ComboBoxYear onChange={() => { }} /></div>
-                        <div>Fin: <ComboBoxYear onChange={() => { }} /></div>
+                        <div>Année: <ComboBoxYear onChange={(year) => setSelectedYear(year)}/></div>
                         <div>
                             Commune:
-                            <select value={selectedCommune} onChange={(e) => setSelectedCommune(e.target.value)}>
-                                <option value="">Select a commune</option>
-                                {communes.map((commune) => (
-                                    <option key={commune} value={commune}>{commune}</option>
-                                ))}
-                            </select>
+                            <Popover open={open} onOpenChange={setOpen}>
+                                <PopoverTrigger asChild>
+                                    <Button
+                                        variant="outline"
+                                        role="combobox"
+                                        aria-expanded={open}
+                                        className="w-[200px] justify-between"
+                                    >
+                                        {selectedCommune || "Select a commune"}
+                                        <ChevronsUpDown className="opacity-50" />
+                                    </Button>
+                                </PopoverTrigger>
+                                <PopoverContent className="w-[200px] p-0">
+                                    <Command>
+                                        <CommandInput placeholder="Search commune..." className="h-9" />
+                                        <CommandList>
+                                            <CommandEmpty>No commune found.</CommandEmpty>
+                                            <CommandGroup>
+                                                {communes.map((commune) => (
+                                                    <CommandItem
+                                                        key={commune}
+                                                        value={commune}
+                                                        onSelect={() => {
+                                                            setSelectedCommune(commune);
+                                                            setOpen(false);
+                                                        }}
+                                                    >
+                                                        {commune}
+                                                        <Check
+                                                            className={cn(
+                                                                "ml-auto",
+                                                                selectedCommune === commune ? "opacity-100" : "opacity-0"
+                                                            )}
+                                                        />
+                                                    </CommandItem>
+                                                ))}
+                                            </CommandGroup>
+                                        </CommandList>
+                                    </Command>
+                                </PopoverContent>
+                            </Popover>
                         </div>
                     </div>
-                    <div ref={mapContainer} className="w-full h-96 rounded-lg" />
+                    <div ref={mapContainer} className="w-full h-[600px] rounded-lg"/>
                 </CardContent>
             </Card>
         </div>
