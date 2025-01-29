05a5676a0364dd7009ebae533cd0bd91a7ad3337
nabilesall <nabilesall@gmail.com>
2025-01-29 15:02:31 +0100

    lines

---
+++ src/app/api/conso-by-year/route.ts
@@ -0,0 +1,26 @@
+const apiUrl = process.env.API_URL
+
+export async function GET(request: Request) {
+
+  try {
+
+    console.log("apiUrl", apiUrl)
+
+    const response = await fetch(`${apiUrl}consommations/stat/conso-by-year`, );
+ 
+    const data = await response.json()
+    
+    return Response.json(data);
+
+  } catch (error) {
+    if (error instanceof Error) {
+      return new Response(`Erreur: ${error.message}`, {
+        status: 500,
+      });
+    } else {
+      return new Response('Erreur inconnue', {
+        status: 500,
+      });
+    }
+  }
+}
