145b75218220fd4b41df583fda95b2e65888d103
Nath <nathan.girardi@proton.me>
2025-01-29 18:25:41 +0100

    RadarChart

--- src/app/graphes/page.tsx
+++ src/app/graphes/page.tsx
@@ -22,6 +22,8 @@ export default function Graph() {
     const [startYear, setStartYear] = useState("2018");
     const [endYear, setEndYear] = useState("2019");
     const [error, setError] = useState("");
+
+
     const [consoElectDoughnut, setConsoElectDoughnut] = React.useState<
         { dataConso: number[], labels: string[] }
     >({
@@ -29,13 +31,112 @@ export default function Graph() {
         labels: []
     })
 
-    const [consoGazDoughnut, setConsoGazDoughnut] = React.useState<
-        { dataConso: number[], labels: string[] }
-    >({
+    const [consoGazDoughnut, setConsoGazDoughnut] = React.useState<{ dataConso: number[], labels: string[] }>
+    ({
         dataConso: [],
         labels: []
     })
-    
+
+    interface ArrondissementData {
+        years : number[],
+        labels : number[]
+    }
+
+    const object = {
+        "2018": {
+            "total_conso_elec": [
+                24353.66,
+                48677.28000000001,
+                82273.18000000001,
+                50349.89999999993,
+                46547.68000000001,
+                37959.890000000014,
+                54025.28,
+                51422.04000000001,
+                55903.4
+            ],
+            "total_conso_gaz": [
+                23628.609999999997,
+                36342.19,
+                37261.35999999999,
+                38536.29999999997,
+                111104.97,
+                29024.3,
+                41743.81,
+                71038.23999999999,
+                68463.46999999999
+            ]
+        },
+        "2019": {
+            "total_conso_elec": [
+                25544.70000000002,
+                49594.090000000026,
+                78880.47000000002,
+                56075.48000000002,
+                49704.42999999997,
+                33281.68999999999,
+                55269.00000000003,
+                57740.29000000004,
+                59088.16999999999
+            ],
+            "total_conso_gaz": [
+                22164.97,
+                30687.92,
+                35432.049999999974,
+                45307.28000000001,
+                125498.10999999994,
+                28544.570000000003,
+                36499.250000000015,
+                73538.89000000003,
+                70277.78999999996
+            ]
+        },
+        "2020": {
+            "total_conso_elec": [
+                21550.050000000007,
+                46506.06000000005,
+                48689.779999999984,
+                52788.9,
+                46870.85999999997,
+                47307.420000000006,
+                55315.869999999966,
+                60157.85999999997,
+                56228.62000000001
+            ],
+            "total_conso_gaz": [
+                21131.140000000007,
+                25173.290000000008,
+                33879.77999999999,
+                39335.85000000001,
+                104915.31,
+                28412.42,
+                35856.70999999999,
+                63360.95999999996,
+                59754.38999999999
+            ]
+        },
+        "labels": [
+            "Lyon 1er Arrondissement",
+            "Lyon 2e Arrondissement",
+            "Lyon 3e Arrondissement",
+            "Lyon 4e Arrondissement",
+            "Lyon 5e Arrondissement",
+            "Lyon 6e Arrondissement",
+            "Lyon 7e Arrondissement",
+            "Lyon 8e Arrondissement",
+            "Lyon 9e Arrondissement"
+        ]
+    }
+
+    const [consoArrondissement, setConsoArrondissement] = React.useState<{ years: { [key: string]: number[] }, labels: string[] }>({
+        years: {},
+        labels: []
+    })
+
+    const [consommationData, setConsommationData] = useState({
+
+    });
+
 
     const handleStartYearChange = (year: string) => {
         if (parseInt(year) > parseInt(endYear)) {
@@ -70,7 +171,8 @@ export default function Graph() {
                     console.log("error", data.error);
                     return;
                 }
-                setConsoElectDoughnut(data);
+                console.log(data)
+                setConsoElectDoughnut(data)
             });
         } catch (error) {
             console.error("Error fetching subcategories: ", error);
@@ -103,9 +205,68 @@ export default function Graph() {
         }
     };
 
+    const fecthElectGazArr = async () => {
+        try {
+            await fetch(`/api/elec-gaz-commune`,
+                {
+                    method: "GET",
+                }
+            )
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                if(data.error) {
+                    console.log("error", data.error);
+                    return;
+                }
+                setConsoArrondissement(
+                    {
+                        years: {
+                            "2018": data["2018"],
+                            "2019": data["2019"],
+                            "2020": data["2020"]
+                        },
+                        labels: data.labels
+                    }
+                )
+            });
+        } catch (error) {
+            console.error("Error fetching subcategories: ", error);
+            return [];
+        }
+    };
+
+    const fecthConsoYear = async () => {
+        try {
+            await fetch(`/api/elec-gaz-commune`,
+                {
+                    method: "GET",
+                }
+            )
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                if(data.error) {
+                    console.log("error", data.error);
+                    return;
+                }
+                console.log('lineChart : ', data)
+            });
+        } catch (error) {
+            console.error("Error fetching subcategories: ", error);
+            return [];
+        }
+    };
+
+
+
     React.useEffect(() => {
         fetchStatElecForDoughnut();
         fetchStatGazForDoughnut();
+        fecthElectGazArr();
+        fecthConsoYear();
     }, []);
 
     return (
@@ -148,11 +309,12 @@ export default function Graph() {
                     <CardHeader>
                         <CardTitle className='text-xl'>Consommation d'énergie par arondissement</CardTitle>
                     </CardHeader>
-                    <CardContent>
-                        <RadarCharter startYear={startYear} endYear={endYear}
-                                      rElec={rElec} gElec={gElec} bElec={bElec}
-                                      transparencyElec={transparencyElec}
-                                      rGaz={rGaz} gGaz={gGaz} bGaz={bGaz} transparencyGaz={transparencyGaz}
+                    <CardContent >
+                        <RadarCharter 
+                            startYear={startYear} 
+                            endYear={endYear}
+                        
+                            
                         />
                     </CardContent>
                 </Card>
--- src/components/RadarCharter.tsx
+++ src/components/RadarCharter.tsx
@@ -28,36 +28,30 @@ export const options = {
     },
 };
 
-const labels = ['1er Arr.', '2ème Arr.', '3ème Arr.', '4ème Arr.', '5ème Arr.',
-    "6ème Arr.", "7ème Arr.", "8ième Arr.", "9ième Arr."];
+interface RadarCharter {
+    startYear: string;
+    endYear: string;
+    labels: string[];
+    datasets: number[]
+}
 
-export function RadarCharter({
-                                 startYear,
-                                 endYear,
-                                 rElec,
-                                 gElec,
-                                 bElec,
-                                 transparencyElec,
-                                 rGaz,
-                                 gGaz,
-                                 bGaz,
-                                 transparencyGaz
-                             }) {
+export function RadarCharter({startYear, endYear, labels, datasets}: RadarCharter) {
+
+    useEffect(() => {
+        console.log('le dataset', datasets)
+    }, [])
+
     const [data, setData] = useState({
-        labels,
+        labels: labels,
         datasets: [
             {
                 label: 'Electricité',
                 data: [6, 9, 3, 5, 4, 7, 8, 9, 10],
-                backgroundColor: `rgba(${rElec}, ${gElec}, ${bElec}, ${transparencyElec})`,
-                borderColor: `rgba(${rElec}, ${gElec}, ${bElec}, ${Math.min(transparencyElec + 0.5, 1)})`,
                 borderWidth: 1,
             },
             {
                 label: 'Gaz',
                 data: [3, 2, 6, 3, 5, 8, 4, 5, 7],
-                backgroundColor: `rgba(${rGaz}, ${gGaz}, ${bGaz}, ${transparencyGaz})`,
-                borderColor: `rgba(${rGaz}, ${gGaz}, ${bGaz}, ${Math.min(transparencyGaz + 0.5, 1)})`,
                 borderWidth: 1,
             },
         ],
